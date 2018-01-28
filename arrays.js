1fd4ededa7075cb06d4ef625910eb7f2066a9346
Larry Jr <larry.g3284@yahoo.com>
2018-01-28 04:26:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -35,7 +35,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  return array.slice(1)
+  return array.splice(1)
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
