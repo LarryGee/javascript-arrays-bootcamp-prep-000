83e01ab61cd36397047317da8e8a0515727fa5ba
Larry Jr <larry.g3284@yahoo.com>
2018-01-28 03:40:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -12,5 +12,6 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.push()
+  array.push(element)
+  return array
 }
