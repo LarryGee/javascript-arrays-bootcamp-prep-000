12f9ea8fd3795a137c3fbd5d2c3f7df4570d3a25
Larry Jr <larry.g3284@yahoo.com>
2018-01-28 04:03:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -29,7 +29,7 @@ function accessElementInArray(array, index) {
   return array[index]
 }
 
-function destructivelyRemoveElementFromBeginningOfArray() {
+function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift()
   array
 }
